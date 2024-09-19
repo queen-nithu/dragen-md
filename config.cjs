0be8aa2930b8ebee97b4936ad024b2fb83efef57
dragen md <nimsaravihaga569@gmail.com>
2024-09-19 18:49:29 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "enter-pair",
+  SESSION_ID: process.env.SESSION_ID || "Session~92f9vqPu",
   PREFIX: process.env.PREFIX || '.',
   MODE: process.env.MODE || "public",
 };
