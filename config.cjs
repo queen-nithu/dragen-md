e2159061f5da98e647a21b24353344f22d5d2b90
queen-nithu <nimsaravihaga37@gmail.com>
2024-09-19 19:32:23 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Session~92f9vqPu",
+  SESSION_ID: process.env.SESSION_ID || "Session~GjSWmbL4",
   PREFIX: process.env.PREFIX || '.',
   MODE: process.env.MODE || "public",
 };
